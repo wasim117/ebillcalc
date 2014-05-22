379a6bc43ae497626c1645c10f082362f3bde23c
Wasim Shaikh <wasim117@gmail.com>
2014-05-23 00:32:23 +0530

    some calculation updates

--- js/app.js
+++ js/app.js
@@ -5,11 +5,21 @@ jQuery(document).ready(function () {
         var pastReading = jQuery("#pastReading").val(),
             presentReading = jQuery("#presentReading").val();
         var unitConsumed = presentReading - pastReading;
-        jQuery("#unitConsumed").text(unitConsumed);
-        energyCharges = jQuery("#energyCharges");
+            jQuery("#unitConsumed").text(unitConsumed);
+            energyCharges = jQuery("#energyCharges");
+
         var first50Unit = 3.20 * 50,
             next150Unit = 3.90 * 150;
-        var govDuty = 15;
+        var govDuty = 15,
+            metRent = 10,
+            fixedCharges = 15,
+            fppaFactor = 1.7 ;
+        jQuery("#fixedCharges").text(fixedCharges);
+        jQuery("#meterRent").text(metRent);
+
+        jQuery("#fpppaCharges").text(unitConsumed*fppaFactor);
+
+        
         if (unitConsumed <= 50) {
             energyCharges.text(unitConsumed * 3.00 + "Rs");
         }
@@ -23,7 +33,8 @@ jQuery(document).ready(function () {
 
         var govtDutyCharge = (15 / 100) * energyCharges.text();
                 jQuery("#govDuty").text(govtDutyCharge);
-                jQuery("#finalCharges").text(parseFloat(energyCharges.text()) + parseFloat(govtDutyCharge));
+
+                jQuery("#finalCharges").text(parseInt(energyCharges.text()) + parseInt(govtDutyCharge) + metRent + fixedCharges + (unitConsumed*fppaFactor));
 
         
 
